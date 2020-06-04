211618d1aa11c3f618e6d207c7c56b8949475a4f
Ho Thuy Duong <duong.ht@neo-lab.vn>
2020-06-04 17:24:47 +0700

    declare function

--- src/Example.php
+++ src/Example.php
@@ -3,7 +3,7 @@ namespace App;
 
 class Example
 {
-    public function sluggify($string, $separator = '-', $maxLength = 96)
+    public function clear($string, $separator = '-', $maxLength = 96)
     {
         $title = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $string);
         $title = preg_replace('%[^-/+|\w ]%', '', $title);
@@ -11,5 +11,30 @@ class Example
         $title = preg_replace('/[\/_|+ -]+/', $separator, $title);
 
         return $title;
+    }
+
+    public function contains(string $value)
+    {
+
+    }
+
+    public function peek()
+    {
+
+    }
+
+    public function push(string $value)
+    {
+
+    }
+
+    public function pop()
+    {
+
+    }
+
+    public function isEmpty()
+    {
+
     }
 }
