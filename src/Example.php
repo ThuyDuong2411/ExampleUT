f1adf6e2fa695955e1c74ee6bc1266f419823679
ThuyDuong2411 <thuongduy24111998@gmail.com>
2020-06-06 10:30:57 +0700

    function tests

--- src/Example.php
+++ src/Example.php
@@ -3,38 +3,36 @@ namespace App;
 
 class Example
 {
-    public function clear($string, $separator = '-', $maxLength = 96)
+    public function clear($array)
     {
-        $title = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $string);
-        $title = preg_replace('%[^-/+|\w ]%', '', $title);
-        $title = strtolower(trim(substr($title, 0, $maxLength), '-'));
-        $title = preg_replace('/[\/_|+ -]+/', $separator, $title);
-
-        return $title;
+        unset($array);
+        return empty($array) ? true : false;
     }
 
-    public function contains(string $value)
+    public function contains($array, $value)
     {
-
+        return array_search($value, $array) ? true : false;
     }
 
-    public function peek()
+    public function peek($array)
     {
-
+        return end($array);
     }
 
-    public function push(string $value)
+    public function push($array, $value)
     {
-
+        array_push($array, $value);
+        return $array;
     }
 
-    public function pop()
+    public function pop($array)
     {
-
+        array_pop($array);
+        return $array;
     }
 
-    public function isEmpty()
+    public function isEmpty($array)
     {
-
+        return empty($array) ? true:false;
     }
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,29 +6,172 @@ use App\Example;
 class ExampleTest extends TestCase
 {
     /**
-     * @param string $originalString String to be sluggified
-     * @param string $expectedResult What we expect our slug result to be
+     * @param $array
+     * @param $value
+     * @param $expectedResult
      *
-     * @dataProvider providerTestSluggifyReturnsSluggifiedString
+     * @dataProvider providerTestPushElementToArray
      */
-    public function testSluggifyReturnsSluggifiedString($originalString, $expectedResult)
+    public function testPushElementToArray($array, $value, $expectedResult)
     {
         $url = new Example();
 
-        $result = $url->sluggify($originalString);
+        $result = $url->push($array, $value);
 
         $this->assertEquals($expectedResult, $result);
     }
 
-    public function providerTestSluggifyReturnsSluggifiedString()
+    public function providerTestPushElementToArray()
     {
         return [
-            ['This string will be sluggified', 'this-string-will-be-sluggified'],
-            ['THIS STRING WILL BE SLUGGIFIED', 'this-string-will-be-sluggified'],
-            ['This1 string2 will3 be 44 sluggified10', 'this1-string2-will3-be-44-sluggified10'],
-            ['This! @string#$ %$will ()be "sluggified', 'this-string-will-be-sluggified'],
-            ["Tänk efter nu – förr'n vi föser dig bort", 'tank-efter-nu-forrn-vi-foser-dig-bort'],
-            ['', ''],
+            [array(), 19, array(19)],
+            [array(), 'a', array('a')],
+            [array(1,2,3,4,5), 19, array(1,2,3,4,5,19)],
+            [array(1,2,3,4,5), 'c', array(1,2,3,4,5,'c')],
+            [array(1,'a',3,'b',5), 19, array(1,'a',3,'b',5,19)],
+            [array(1,'a',3,'b',5), 'c', array(1,'a',3,'b',5,'c')],
+            [array('a','b','c','d','e'), 'f', array('a','b','c','d','e','f')],
+            [array('a','b','c','d','e'), 7, array('a','b','c','d','e',7)],
+
+
+            [array(), 19, array()],
+            [array(1,2,3,4,5), 19, array(19)],
+            [array(1,2,3,4,5), 19, array(1,2,3,4,19)],
+            [array(1,2,3,4,5), 19, array(19,2,3,4,5)],
+            [array(1,2,3,4,5), 19, array(19,1,2,3,4,5)],
+            [array(1,2,3,4,5), 19, array(1,2,3,19,4,5)],
+        ];
+    }
+
+    /**
+     * @param $array
+     * @param $expectedResult
+     *
+     * @dataProvider providerTestPopElementFromArray
+     */
+    public function testPopElementFromArray($array, $expectedResult)
+    {
+        $url = new Example();
+
+        $result = $url->pop($array);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function providerTestPopElementFromArray()
+    {
+        return [
+            [array(), array()],
+            [array(1,2,3,4,5), array(1,2,3,4)],
+            [array('a','b','c','d','e'), array('a','b','c','d')],
+            [array(1,'a',3,'b',5), array(1,'a',3,'b')],
+
+            [array(1,2,3,4,5), array(2,3,4,5)],
+            [array(1,2,3,4,5), array(1,2,4,5)],
+        ];
+    }
+
+    /**
+     * @param $array
+     * @param $expectedResult
+     *
+     * @dataProvider providerTestIsEmptyArray
+     */
+    public function testIsEmptyArray($array)
+    {
+        $url = new Example();
+
+        $result = $url->isEmpty($array);
+
+        $this->assertTrue($result);
+    }
+
+    public function providerTestIsEmptyArray()
+    {
+        return [
+            [array()],
+
+            [array(1,2,3,4,5)],
+            [array(1,'b','c',4,5)],
+            [array('a','b','c','d','e')],
+        ];
+    }
+
+    /**
+     * @param $array
+     * @param $expectedResult
+     *
+     * @dataProvider providerTestPeekFromArray
+     */
+    public function testPeekFromArray($array, $expectedResult)
+    {
+        $url = new Example();
+
+        $result = $url->peek($array);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function providerTestPeekFromArray()
+    {
+        return [
+            [array(), false],
+            [array(1,2,3,4,5), 5],
+            [array(1,'b',4,5, 'c'), 'c'],
+            [array('a','b','c','d','e'), 'e'],
+
+            [array(1,2,3,4,5), 1],
+            [array(1,2,3,4,5), 3],
+        ];
+    }
+
+    /**
+     * @param $array
+     *
+     * @dataProvider providerTestClearArray
+     */
+    public function testClearArray($array)
+    {
+        $url = new Example();
+
+        $result = $url->clear($array);
+
+        $this->assertTrue($result);
+    }
+
+    public function providerTestClearArray()
+    {
+        return [
+            [array()],
+            [array(1,2,3,4,5)],
+            [array(1,'b',4,5, 'c')],
+            [array('a','b','c','d','e')],
+        ];
+    }
+
+    /**
+     * @param $array
+     * @param $expectedResult
+     *
+     * @dataProvider providerTestArrayContainsElement
+     */
+    public function testArrayContainsElement($array, $value, $expectedResult)
+    {
+        $url = new Example();
+
+        $result = $url->contains($array, $value);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function providerTestArrayContainsElement()
+    {
+        return [
+            [array(), 8, false],
+            [array(1,2,3,4,5), 5, 4],
+            [array(1,'b',4, 5, 'c'), 'b', 1],
+            [array('a','b','c','d','e'), 'c', 2],
+            [array(1,2,3,4,5), 6, false],
         ];
     }
 }
