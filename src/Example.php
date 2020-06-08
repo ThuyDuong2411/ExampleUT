0e27f834e64404f7af9449730c703fd065729f87
Ho Thuy Duong <duong.ht@neo-lab.vn>
2020-06-08 14:03:55 +0700

    change code function

--- src/Example.php
+++ src/Example.php
@@ -3,36 +3,131 @@ namespace App;
 
 class Example
 {
-    public function clear($array)
+    protected $capacity;
+    protected $stack = [];
+    protected $top;
+
+    public function __construct($capacity, $stack)
     {
-        unset($array);
-        return empty($array) ? true : false;
+        $this->capacity = $capacity;
+        $this->stack = $stack;
     }
 
-    public function contains($array, $value)
+    public function getTop()
     {
-        return array_search($value, $array) ? true : false;
+        return $this->top = count($this->stack) - 1;
     }
 
-    public function peek($array)
+    public function validate()
     {
-        return end($array);
+        $error = '';
+        if (!is_array($this->stack) && $this->capacity < 0) {
+            $error = "Please check stack and capacity.";
+        } else if (!is_array($this->stack)) {
+            $error = "Stack is not an array.";
+        } else if ($this->capacity < 0) {
+            $error = "Capacity is invalid.";
+        } else if (count($this->stack) > $this->capacity) {
+            $error = "Stack is over.";
+        }
+        return $error;
     }
 
-    public function push($array, $value)
+    public function isEmpty()
     {
-        array_push($array, $value);
-        return $array;
+        if ($error = $this->validate()) {
+            return $error;
+        }
+        if ($this->getTop() == -1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public function pop($array)
+    public function isFull()
     {
-        array_pop($array);
-        return $array;
+        if ($this->getTop() >= $this->capacity - 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public function isEmpty($array)
+    public function push($value)
     {
-        return empty($array) ? true:false;
+        if ($error = $this->validate()) {
+            return $error;
+        }
+        if ($this->isFull()) {
+            return "Stack is full.";
+        } else {
+            $this->top = $this->getTop();
+            ++$this->top;
+            $this->stack[$this->top] = $value;
+            return $this->stack;
+        }
+    }
+
+    public function pop()
+    {
+        if ($error = $this->validate()) {
+            return $error;
+        }
+        if ($this->isEmpty()) {
+            return "Stack is empty.";
+        } else {
+            $this->top = $this->getTop();
+            unset($this->stack[$this->top]);
+            return $this->stack;
+        }
+    }
+
+    public function peek()
+    {
+        if ($error = $this->validate()) {
+            return $error;
+        }
+        if ($this->isEmpty()) {
+            return "Stack is empty.";
+        } else {
+            $this->top = $this->getTop();
+            return $this->stack[$this->top];
+        }
+    }
+
+    public function clear()
+    {
+        if ($error = $this->validate()) {
+            return $error;
+        }
+        if ($this->isEmpty()) {
+            return "Stack is empty.";
+        } else {
+            $count = count($this->stack);
+            for ($i = 0; $i < $count; $i++) {
+                unset($this->stack[$i]);
+            }
+            return $this->isEmpty() ? true : false;
+        }
+    }
+
+    public function contains($value)
+    {
+        if ($error = $this->validate()) {
+            return $error;
+        }
+        if ($this->isEmpty()) {
+            return "Stack is empty.";
+        } else {
+            $count = count($this->stack);
+            for ($i = 0; $i < $count; $i++) {
+                if ($value == $this->stack[$i]) {
+                    $index = $i + 1;
+                    return "Element at index $index in stack.";
+                }
+            }
+            return "No element in stack.";
+        }
     }
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,17 +6,50 @@ use App\Example;
 class ExampleTest extends TestCase
 {
     /**
-     * @param $array
+     * @param $capacity
+     * @param $stack
+     * @param $expectedResult
+     *
+     * @dataProvider providerTestIsEmptyStack
+     */
+    public function testIsEmptyStack($capacity, $stack, $expectedResult)
+    {
+        $url = new Example($capacity, $stack);
+
+        $result = $url->isEmpty();
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function providerTestIsEmptyStack()
+    {
+        return [
+            [5, array(), true],
+            [0, array(), true],
+
+            [5, array(1,2,3,4,5,6), "Stack is over."],
+            [0, '', "Stack is not an array."],
+            [5, 8, "Stack is not an array."],
+            [-3, array(), "Capacity is invalid."],
+            [-3, '', "Please check stack and capacity."],
+            [5, array(2), false],
+            [5, array('s','b'), false],
+        ];
+    }
+
+    /**
+     * @param $capacity
+     * @param $stack
      * @param $value
      * @param $expectedResult
      *
      * @dataProvider providerTestPushElementToArray
      */
-    public function testPushElementToArray($array, $value, $expectedResult)
+    public function testPushElementToArray($capacity, $stack, $value, $expectedResult)
     {
-        $url = new Example();
+        $url = new Example($capacity, $stack);
 
-        $result = $url->push($array, $value);
+        $result = $url->push($value);
 
         $this->assertEquals($expectedResult, $result);
     }
@@ -24,36 +57,35 @@ class ExampleTest extends TestCase
     public function providerTestPushElementToArray()
     {
         return [
-            [array(), 19, array(19)],
-            [array(), 'a', array('a')],
-            [array(1,2,3,4,5), 19, array(1,2,3,4,5,19)],
-            [array(1,2,3,4,5), 'c', array(1,2,3,4,5,'c')],
-            [array(1,'a',3,'b',5), 19, array(1,'a',3,'b',5,19)],
-            [array(1,'a',3,'b',5), 'c', array(1,'a',3,'b',5,'c')],
-            [array('a','b','c','d','e'), 'f', array('a','b','c','d','e','f')],
-            [array('a','b','c','d','e'), 7, array('a','b','c','d','e',7)],
+            [5, array(), 10, array(10)],
+            [5, array(), 'a', array('a')],
+            [5, array(1,2,3,4), 10, array(1,2,3,4,10)],
+            [5, array(1,2,3,4), 'c', array(1,2,3,4,'c')],
+            [5, array(1,'a',3,'b'), 10, array(1,'a',3,'b',10)],
+            [5, array(1,'a',3,'b'), 'c', array(1,'a',3,'b','c')],
+            [5, array('a','b','c','d'), 'f', array('a','b','c','d','f')],
+            [5, array('a','b','c','d'), 7, array('a','b','c','d',7)],
 
-
-            [array(), 19, array()],
-            [array(1,2,3,4,5), 19, array(19)],
-            [array(1,2,3,4,5), 19, array(1,2,3,4,19)],
-            [array(1,2,3,4,5), 19, array(19,2,3,4,5)],
-            [array(1,2,3,4,5), 19, array(19,1,2,3,4,5)],
-            [array(1,2,3,4,5), 19, array(1,2,3,19,4,5)],
+            [5, array(1,2,3,4,5,6), 10, "Stack is over."],
+            [5, array(1,2,3,4,5), 10, "Stack is full."],
+            [-7, array(1,2,3,4), 10, "Capacity is invalid."],
+            [5, 9, 10, "Stack is not an array."],
+            [-3, '', 10, "Please check stack and capacity."],
         ];
     }
 
     /**
-     * @param $array
+     * @param $capacity
+     * @param $stack
      * @param $expectedResult
      *
      * @dataProvider providerTestPopElementFromArray
      */
-    public function testPopElementFromArray($array, $expectedResult)
+    public function testPopElementFromArray($capacity, $stack, $expectedResult)
     {
-        $url = new Example();
+        $url = new Example($capacity, $stack);
 
-        $result = $url->pop($array);
+        $result = $url->pop();
 
         $this->assertEquals($expectedResult, $result);
     }
@@ -61,53 +93,30 @@ class ExampleTest extends TestCase
     public function providerTestPopElementFromArray()
     {
         return [
-            [array(), array()],
-            [array(1,2,3,4,5), array(1,2,3,4)],
-            [array('a','b','c','d','e'), array('a','b','c','d')],
-            [array(1,'a',3,'b',5), array(1,'a',3,'b')],
+            [5, array(1,2,3), array(1,2)],
+            [5, array('a','b','c','d'), array('a','b','c')],
+            [5, array(1,'a',3,'b'), array(1,'a',3)],
 
-            [array(1,2,3,4,5), array(2,3,4,5)],
-            [array(1,2,3,4,5), array(1,2,4,5)],
+            [5, array(1,2,3,4,5,6), "Stack is over."],
+            [5, array(), "Stack is empty."],
+            [-7, array(1,2,3,4), "Capacity is invalid."],
+            [5, 9, "Stack is not an array."],
+            [-3, '', "Please check stack and capacity."],
         ];
     }
 
     /**
-     * @param $array
-     * @param $expectedResult
-     *
-     * @dataProvider providerTestIsEmptyArray
-     */
-    public function testIsEmptyArray($array)
-    {
-        $url = new Example();
-
-        $result = $url->isEmpty($array);
-
-        $this->assertTrue($result);
-    }
-
-    public function providerTestIsEmptyArray()
-    {
-        return [
-            [array()],
-
-            [array(1,2,3,4,5)],
-            [array(1,'b','c',4,5)],
-            [array('a','b','c','d','e')],
-        ];
-    }
-
-    /**
-     * @param $array
+     * @param $capacity
+     * @param $stack
      * @param $expectedResult
      *
      * @dataProvider providerTestPeekFromArray
      */
-    public function testPeekFromArray($array, $expectedResult)
+    public function testPeekFromArray($capacity, $stack, $expectedResult)
     {
-        $url = new Example();
+        $url = new Example($capacity, $stack);
 
-        $result = $url->peek($array);
+        $result = $url->peek();
 
         $this->assertEquals($expectedResult, $result);
     }
@@ -115,51 +124,62 @@ class ExampleTest extends TestCase
     public function providerTestPeekFromArray()
     {
         return [
-            [array(), false],
-            [array(1,2,3,4,5), 5],
-            [array(1,'b',4,5, 'c'), 'c'],
-            [array('a','b','c','d','e'), 'e'],
+            [5, array(1,2,3), 3],
+            [5, array('a','b','c','d'), 'd'],
+            [5, array(1,'a',3,'b'), 'b'],
 
-            [array(1,2,3,4,5), 1],
-            [array(1,2,3,4,5), 3],
+            [5, array(1,2,3,4,5,6), "Stack is over."],
+            [5, array(), "Stack is empty."],
+            [-7, array(1,2,3,4), "Capacity is invalid."],
+            [5, 9, "Stack is not an array."],
+            [-3, '', "Please check stack and capacity."],
         ];
     }
 
     /**
-     * @param $array
+     * @param $capacity
+     * @param $stack
+     * @param $expectedResult
      *
      * @dataProvider providerTestClearArray
      */
-    public function testClearArray($array)
+    public function testClearArray($capacity, $stack, $expectedResult)
     {
-        $url = new Example();
+        $url = new Example($capacity, $stack);
 
-        $result = $url->clear($array);
+        $result = $url->clear();
 
-        $this->assertTrue($result);
+        $this->assertEquals($result, $expectedResult);
     }
 
     public function providerTestClearArray()
     {
         return [
-            [array()],
-            [array(1,2,3,4,5)],
-            [array(1,'b',4,5, 'c')],
-            [array('a','b','c','d','e')],
+            [5, array(1,2,3,4), true],
+            [5, array(1,'b',4,5), true],
+            [5, array('a','b','c','d','e'), true],
+
+            [5, array(1,2,3,4,5,6), "Stack is over."],
+            [5, array(), "Stack is empty."],
+            [-7, array(1,2,3,4), "Capacity is invalid."],
+            [5, 9, "Stack is not an array."],
+            [-3, '', "Please check stack and capacity."],
         ];
     }
 
     /**
-     * @param $array
+     * @param $capacity
+     * @param $stack
+     * @param $value
      * @param $expectedResult
      *
      * @dataProvider providerTestArrayContainsElement
      */
-    public function testArrayContainsElement($array, $value, $expectedResult)
+    public function testArrayContainsElement($capacity, $stack, $value, $expectedResult)
     {
-        $url = new Example();
+        $url = new Example($capacity, $stack);
 
-        $result = $url->contains($array, $value);
+        $result = $url->contains($value);
 
         $this->assertEquals($expectedResult, $result);
     }
@@ -167,11 +187,16 @@ class ExampleTest extends TestCase
     public function providerTestArrayContainsElement()
     {
         return [
-            [array(), 8, false],
-            [array(1,2,3,4,5), 5, 4],
-            [array(1,'b',4, 5, 'c'), 'b', 1],
-            [array('a','b','c','d','e'), 'c', 2],
-            [array(1,2,3,4,5), 6, false],
+            [5, array(1,2,3,4), 3, "Element at index 3 in stack."],
+            [5, array(1,'b',4, 5, 'c'), 'b', "Element at index 2 in stack."],
+            [5, array('a','b','c','d','e'), 'c', "Element at index 3 in stack."],
+
+            [5, array(1,2,3,4,5), 6, "No element in stack."],
+            [5, array(), 8, "Stack is empty."],
+            [5, array(1,2,3,4,5,6), 9, "Stack is over."],
+            [-7, array(1,2,3,4), 8, "Capacity is invalid."],
+            [5, 9, 6, "Stack is not an array."],
+            [-3, '', 7, "Please check stack and capacity."],
         ];
     }
 }
